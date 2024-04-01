68760b00e0e43311a37951a488cd068a418eee76
siamCSEjnu <99203101+siamCSEJnU@users.noreply.github.com>
2024-04-02 02:45:10 +0600

    add info page created

--- src/pages/Create/Create.jsx
+++ src/pages/Create/Create.jsx
@@ -3,7 +3,93 @@ import React from "react";
 const Create = () => {
   return (
     <div>
-      <p>This is create page</p>
+      <p className="text-4xl font-semibold text-sky-700 text-center mt-16 mb-8">
+        Add New Student Info
+      </p>
+      <div className="w-1/3 mx-auto bg-sky-200 p-4">
+        <form>
+          <div className="mb-4">
+            <label
+              htmlFor="name"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Enter name"
+              className="border rounded-md px-3 py-2 w-full outline-none"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="class"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              Class
+            </label>
+            <select
+              id="class"
+              name="class"
+              className="border rounded-md px-3 py-2 w-full"
+            >
+              <option value="">Select Class</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+              <option value="4">Four</option>
+              <option value="5">Five</option>
+              <option value="6">Six</option>
+              <option value="7">Seven</option>
+              <option value="8">Eight</option>
+              <option value="9">Nine</option>
+              <option value="10">Ten</option>
+            </select>
+          </div>
+          <div className="mb-4">
+            <label htmlFor="dob" className="block text-gray-700 font-bold mb-2">
+              Date of Birth
+            </label>
+            <input
+              type="date"
+              id="dob"
+              name="dob"
+              className="border rounded-md px-3 py-2 w-full outline-none"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 font-bold">Gender</label>
+            <div>
+              <label className="inline-flex items-center">
+                <input
+                  type="radio"
+                  name="gender"
+                  value="male"
+                  className="form-radio"
+                />
+                <span className="ml-2">Male</span>
+              </label>
+              <label className="inline-flex items-center ml-6">
+                <input
+                  type="radio"
+                  name="gender"
+                  value="female"
+                  className="form-radio"
+                />
+                <span className="ml-2">Female</span>
+              </label>
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,83 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import deleteIcon from "../../assets/icons/deleteIcon.png";
+import detailsIcon from "../../assets/icons/detailsIcon.png";
+import editIcon from "../../assets/icons/editIcon.png";
 
 const Home = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:5000/students").then((res) => {
+      const formattedData = res.data.map((d) => ({
+        ...d,
+        DOB: formatDate(d.DOB),
+      }));
+      setData(formattedData);
+      console.log(formattedData);
+    });
+  }, []);
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = date.getFullYear();
+    return `${day}-${month}-${year}`;
+  };
+
   return (
     <div>
-      <div className="px-52">
-        <p className="text-center text-4xl my-12">
+      <div className=" w-2/5 mx-auto">
+        <p className="text-center text-4xl my-12 font-semibold text-sky-600">
           Dhaka Regency Hotel & Resort
         </p>
-        <Link to="/create">
-          <button>Add New Student Info</button>
-        </Link>
+        <p className="pb-5">
+          <Link to="/create">
+            <button className="btn py-1 px-2 font-semibold rounded-md hover:bg-lime-600 hover:text-slate-800 hover:border-lime-600  text-slate-200 bg-slate-800 border-slate-800">
+              Add New Student Info
+            </button>
+          </Link>
+        </p>
+
+        <table className="table-auto w-full p-3">
+          <thead>
+            <tr className="bg-slate-400">
+              <th className="border border-slate-200 p-2">Name</th>
+              <th className="border border-slate-200 p-2">Class</th>
+              <th className="border border-slate-200 p-2">DOB</th>
+              <th className="border border-slate-200 p-2">Gender</th>
+              <th className="border border-slate-200 p-2">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data?.map((d, i) => (
+              <tr key={i} className="bg-orange-200 ">
+                <td className="p-2 border border-slate-200">{d.StudentName}</td>
+                <td className="text-center border border-slate-200 p-2">
+                  {d.ClassName}
+                </td>
+                <td className="text-center border border-slate-200 p-2">
+                  {d.DOB}
+                </td>
+                <td className="text-center border border-slate-200 p-2">
+                  {d.Gender == 1 ? "Male" : "Female"}
+                </td>
+                <td className="flex justify-center gap-2 border border-slate-200 p-2">
+                  <div>
+                    {" "}
+                    <img src={detailsIcon} alt="Details" className="w-8" />{" "}
+                  </div>
+                  <div>
+                    <img src={editIcon} alt="Edit" className="w-8" />
+                  </div>{" "}
+                  <div>
+                    <img src={deleteIcon} alt="Delete" className="w-8" />{" "}
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
